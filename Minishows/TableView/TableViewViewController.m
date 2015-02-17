8fb3cd4a86c155c2547e549d1cccdafb7521b738
Julian <julian.alonso.dev@gmail.com>
2015-02-17 15:37:38 +0100

    Added tableviewcontroller and xib

---
+++ Minishows/TableView/TableViewViewController.m
@@ -0,0 +1,37 @@
+//
+//  TableViewViewController.m
+//  Minishows
+//
+//  Created by Julian Alonso on 17/2/15.
+//  Copyright (c) 2015 IronHack. All rights reserved.
+//
+
+#import "TableViewViewController.h"
+
+@interface TableViewViewController ()
+
+@end
+
+@implementation TableViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
