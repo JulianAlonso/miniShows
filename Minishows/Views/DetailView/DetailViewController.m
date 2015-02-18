84e332113aebb2ac87b0c9e0a326bc004eadd64a
Julian <julian.alonso.dev@gmail.com>
2015-02-18 19:45:45 +0100

    Animating uiview to original size

--- Minishows/Views/DetailView/DetailViewController.m
+++ Minishows/Views/DetailView/DetailViewController.m
@@ -8,11 +8,16 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
+@interface DetailViewController () <UIScrollViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *serieImageView;
 @property (weak, nonatomic) IBOutlet UILabel *generalInfoLabel;
 @property (weak, nonatomic) IBOutlet UILabel *overviewLabel;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *heightConstraint;
+
+@property (assign, nonatomic) CGFloat originalConstant;
 
 @end
 
@@ -20,6 +25,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.scrollView.delegate = self;
+    
+    self.originalConstant = self.heightConstraint.constant;
     
     [self loadProperties];
     
@@ -30,6 +39,45 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - ScrollViewDelegate methods.
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    if (!decelerate)
+    {
+        [self returnConstantToOriginalHeight];
+    }
+
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    [self returnConstantToOriginalHeight];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat move = self.scrollView.contentOffset.y;
+    CGFloat diferencia = self.heightConstraint.constant - self.originalConstant;
+    if (move < -64)
+    {
+        if (diferencia < 100)
+        {
+            self.heightConstraint.constant += 4;
+        }
+        else if (diferencia < 200)
+        {
+            self.heightConstraint.constant += 2;
+        }
+        else
+        {
+            self.heightConstraint.constant += 1;
+        }
+
+        NSLog(@"Diferencia: %f", diferencia);
+    }
+    
+}
+
 #pragma mark - Own methods.
 - (void)loadProperties
 {
@@ -38,5 +86,14 @@
     self.overviewLabel.text = self.serie.overview;
 }
 
-
+- (void)returnConstantToOriginalHeight
+{
+    if (self.heightConstraint.constant > self.originalConstant)
+    {
+        self.heightConstraint.constant = self.originalConstant;
+        [UIView animateWithDuration:0.2f animations:^{
+            [self.scrollView layoutIfNeeded];
+        }];
+    }
+}
 @end
